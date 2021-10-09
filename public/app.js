d95c8e8dc07674f100f40b8e8e68f794eaaf19fb
Mike Melgren <mikemelgren@gmail.com>
2021-10-08 23:06:59 -0500

    added new functions to select playlists

--- public/app.js
+++ public/app.js
@@ -1,15 +1,101 @@
 
-//remove beginning of hash fragment / query string
-const hash = window.location.hash.substring(14)
+const selectedPlaylists = []
 
-//remove end of hash fragment / query string
-const auth = "Bearer " + hash.substring(0, hash.indexOf('&'))
+//function to get playlist from spotify API
+async function getPlaylists(user) {
+    const response = await fetch(`https://api.spotify.com/v1/users/${user}/playlists`, {
+        method: 'GET',
+        headers: {
+            "Authorization": "Bearer " + access_token
+        }
+    })
+    const data = await response.json()
+    const playlists = data.items
+    return playlists
+}
+
+//function to display users playlists
+async function displayPlaylists() {
+    const user = document.querySelector("#user").value;
+    const lists = await getPlaylists(user)
+    const ol = document.querySelector("#playlists")
+    //empty out previous playlists when user is switched
+    while (ol.firstChild) { ol.removeChild(ol.firstChild) }
+    //loop over all the playlists for selected user
+    for (x in lists) {
+        const li = document.createElement("li")
+        const a = document.createElement("a")
+
+        const listName = lists[x].name
+        const listID = lists[x].id
+
+        a.innerHTML = listName
+        //add event listener to handle selected playlists
+        a.addEventListener("click", function selectPlaylist() {
+            //display heading 
+            document.querySelector("#selected-heading").style.display = 'block'
+            //send playlist name and ID to selectedPlaylists array
+            const newPlaylistObject = { name: listName, id: listID }
+            selectedPlaylists.push(newPlaylistObject);
+
+            const ol = document.querySelector("#selected-playlists")
+            const li = document.createElement("li")
+
+            const cancel = document.createElement("a")
+            cancel.innerHTML = "X"
+            cancel.setAttribute("class", "cancel")
+            cancel.addEventListener('click', function cancelPlaylist() {
+                this.parentNode.remove()
+                selectedPlaylists.splice(selectedPlaylists.indexOf(newPlaylistObject), 1)
+                if (selectedPlaylists.length == 0) {
+                    document.querySelector("#selected-heading").style.display = 'none'
+                }
+            })
+
+
+            li.innerHTML = listName
+            li.appendChild(cancel)
+            ol.appendChild(li)
+        })
+        li.appendChild(a)
+        ol.appendChild(li)
+    }
+}
+
+function newUser() {
+
+
+    //Select form area on page
+    const form = document.getElementById("form")
+    const position = form.childElementCount;
+
+    //counter variable
+    const count = (form.childElementCount - 1) / 2
+
+    if (count <= 5) {
+
+        //create new input field
+        const new_user = document.createElement("input")
+        new_user.setAttribute("type", "text")
+        new_user.setAttribute("name", `input_${count}`)
+        new_user.setAttribute("id", `input_${count}`)
+        //create new input label
+        const new_label = document.createElement("label")
+        new_label.textContent = `User #${count}`
+        new_label.setAttribute("for", `input_${count}`)
+        //insert mew field and label
+
+        form.insertBefore(new_user, form.childNodes[position]);
+        form.insertBefore(new_label, form.childNodes[position]);
+    }
+}
+
+//   <label for="newplay">New playlist name: </label>
 
 //function that takes the input from form on submit, slices the unneeded parts and assigns to variable
-async function playlistCombine() {
+async function combinePlaylist() {
     const play1 = document.querySelector("#play1").value.slice(34, 56)
     const play2 = document.querySelector("#play2").value.slice(34, 56)
-    const user = document.getElementById('display-name').textContent
 
     //call the playlist fetch function for each input playlist
     const playlist1 = await playlistFetch(play1)
@@ -36,29 +122,16 @@ async function playlistCombine() {
     }
 
     //create a new playlist, and add new songs to playlist
-    createPlaylist(user, trackList)
-}
-
-//function to get playlist from spotify API
-async function playlistFetch(listname) {
-    const response = await fetch('https://api.spotify.com/v1/playlists/' + listname + '/tracks', {
-        method: 'GET',
-        headers: {
-            "Authorization": auth
-        }
-    })
-    const tracks = await response.json()
-        .catch(alert("Invalid playlist, please double check url"))
-    return tracks
+    createPlaylist(user1, trackList)
 }
 
 //function to create a new playlist with spotify API
-async function createPlaylist(userID, tracks) {
+async function createPlaylist(user, tracks) {
     const newplay = document.querySelector("#newplay").value
-    const response = await fetch('https://api.spotify.com/v1/users/' + userID + '/playlists', {
+    const response = await fetch(`https://api.spotify.com/v1/users/${user}/playlists`, {
         method: 'POST',
         headers: {
-            "Authorization": auth
+            "Authorization": "Bearer " + access_token
         },
         body: JSON.stringify({
             "name": newplay,
@@ -67,20 +140,17 @@ async function createPlaylist(userID, tracks) {
         })
     })
     const data = await response.json()
-    userName = data.owner.display_name
     playlistID = data.id
-
-    alert('New playlist named "' + newplay + '" created for user: ' + userName)
-
+    alert('New playlist named "' + newplay + '" created for user: ' + userID)
     addToPlaylist(playlistID, tracks)
 }
 
 //function to add new songs to the new playlist with spotify API
 function addToPlaylist(playlistID, tracks) {
-    fetch('https://api.spotify.com/v1/playlists/' + playlistID + '/tracks', {
+    fetch(`https://api.spotify.com/v1/playlists/${playlistID}/tracks`, {
         method: 'POST',
         headers: {
-            "Authorization": auth
+            "Authorization": "Bearer " + access_token
         },
         body: JSON.stringify({ "uris": tracks })
     })
---
+++ public/auth.js
@@ -0,0 +1,99 @@
+let user1
+let access_token
+
+(function () {
+
+    var stateKey = 'spotify_auth_state';
+
+    /**
+     * Obtains parameters from the hash of the URL
+     * @return Object
+     */
+    function getHashParams() {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g,
+            q = window.location.hash.substring(1);
+        while (e = r.exec(q)) {
+            hashParams[e[1]] = decodeURIComponent(e[2]);
+        }
+        return hashParams;
+    }
+
+    /**
+     * Generates a random string containing numbers and letters
+     * @param  {number} length The length of the string
+     * @return {string} The generated string
+     */
+    function generateRandomString(length) {
+        var text = '';
+        var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+        for (var i = 0; i < length; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+    };
+
+    var userProfileSource = document.getElementById('user-profile-template').innerHTML,
+        userProfileTemplate = Handlebars.compile(userProfileSource),
+        userProfilePlaceholder = document.getElementById('user-profile');
+
+    oauthSource = document.getElementById('oauth-template').innerHTML,
+        oauthTemplate = Handlebars.compile(oauthSource),
+        oauthPlaceholder = document.getElementById('oauth');
+
+    var params = getHashParams();
+
+    access_token = params.access_token,
+        state = params.state,
+        storedState = localStorage.getItem(stateKey);
+
+    if (access_token && (state == null || state !== storedState)) {
+        alert('Authentication error, please retry');
+    } else {
+        localStorage.removeItem(stateKey);
+        if (access_token) {
+            $.ajax({
+                url: 'https://api.spotify.com/v1/me',
+                headers: {
+                    'Authorization': 'Bearer ' + access_token
+                },
+                success: function (response) {
+                    userProfilePlaceholder.innerHTML = userProfileTemplate(response);
+                    user1 = response.display_name
+                    document.getElementById("user").value = user1;
+                    $('#login').hide();
+                    $('#loggedin').show();
+                    $('header').show();
+                }
+            });
+        } else {
+            $('#login').show();
+            $('#loggedin').hide();
+            $('header').hide();
+        }
+
+        if (document.querySelector("#login").style.display == "block") {
+
+            window.addEventListener('load', function () {
+
+                var client_id = 'f4d501136b224ab4b3827b1e63e3e047'; // Your client id
+                var redirect_uri = 'http://localhost:8888'; // Your redirect uri
+
+                var state = generateRandomString(16);
+
+                localStorage.setItem(stateKey, state);
+                var scope = 'user-read-private user-read-email playlist-modify-public playlist-modify-private';
+
+                var url = 'https://accounts.spotify.com/authorize';
+                url += '?response_type=token';
+                url += '&client_id=' + encodeURIComponent(client_id);
+                url += '&scope=' + encodeURIComponent(scope);
+                url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+                url += '&state=' + encodeURIComponent(state);
+
+                window.location = url;
+            }, false);
+        }
+    }
+})();
