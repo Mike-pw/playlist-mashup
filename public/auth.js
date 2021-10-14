f1aefcf8ff354dd62317e023ca16844ac5d36303
Mike Melgren <mikemelgren@gmail.com>
2021-10-14 13:07:47 -0500

    removed handlebars

--- public/auth.js
+++ public/auth.js
@@ -34,13 +34,13 @@ let access_token
         return text;
     };
 
-    var userProfileSource = document.getElementById('user-profile-template').innerHTML,
-        userProfileTemplate = Handlebars.compile(userProfileSource),
-        userProfilePlaceholder = document.getElementById('user-profile');
+    //var userProfileSource = document.getElementById('user-profile-template').innerHTML,
+    //    userProfileTemplate = Handlebars.compile(userProfileSource),
+    //    userProfilePlaceholder = document.getElementById('user-profile');
 
-    oauthSource = document.getElementById('oauth-template').innerHTML,
-        oauthTemplate = Handlebars.compile(oauthSource),
-        oauthPlaceholder = document.getElementById('oauth');
+    //oauthSource = document.getElementById('oauth-template').innerHTML,
+    //    oauthTemplate = Handlebars.compile(oauthSource),
+    //    oauthPlaceholder = document.getElementById('oauth');
 
     var params = getHashParams();
 
@@ -59,7 +59,7 @@ let access_token
                     'Authorization': 'Bearer ' + access_token
                 },
                 success: function (response) {
-                    userProfilePlaceholder.innerHTML = userProfileTemplate(response);
+                    //                 userProfilePlaceholder.innerHTML = userProfileTemplate(response);
                     user = response.display_name
                     document.getElementById("user").value = user;
                     $('#login').hide();
@@ -73,27 +73,25 @@ let access_token
             $('header').hide();
         }
 
-        if (document.querySelector("#login").style.display == "block") {
+        document.querySelector("#login-button").addEventListener('click', function () {
 
-            window.addEventListener('load', function () {
+            var client_id = 'f4d501136b224ab4b3827b1e63e3e047'; // Your client id
+            var redirect_uri = 'http://localhost:8888'; // Your redirect uri
 
-                var client_id = 'f4d501136b224ab4b3827b1e63e3e047'; // Your client id
-                var redirect_uri = 'http://localhost:8888'; // Your redirect uri
+            var state = generateRandomString(16);
 
-                var state = generateRandomString(16);
+            localStorage.setItem(stateKey, state);
+            var scope = 'user-read-private user-read-email playlist-modify-public playlist-modify-private';
 
-                localStorage.setItem(stateKey, state);
-                var scope = 'user-read-private user-read-email playlist-modify-public playlist-modify-private';
+            var url = 'https://accounts.spotify.com/authorize';
+            url += '?response_type=token';
+            url += '&client_id=' + encodeURIComponent(client_id);
+            url += '&scope=' + encodeURIComponent(scope);
+            url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+            url += '&state=' + encodeURIComponent(state);
 
-                var url = 'https://accounts.spotify.com/authorize';
-                url += '?response_type=token';
-                url += '&client_id=' + encodeURIComponent(client_id);
-                url += '&scope=' + encodeURIComponent(scope);
-                url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
-                url += '&state=' + encodeURIComponent(state);
+            window.location = url;
+        }, false);
 
-                window.location = url;
-            }, false);
-        }
     }
 })();
