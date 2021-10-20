70784311f2eb977e1a6b5bd1c9f97a173eec0977
Mike Melgren <mikemelgren@gmail.com>
2021-10-20 14:00:31 -0500

    service worker

--- public/app.js
+++ public/app.js
@@ -1,3 +1,10 @@
+// Register service worker to control making site work offline
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js')
+        .then(() => { console.log('Service Worker Registered'); });
+}
 
 const selectedPlaylists = []
 
